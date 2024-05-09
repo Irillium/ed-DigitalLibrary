908a4ac96164b405f7fd6048e67f49a47a36ee48
Iris Ruiz <146753865+Irillium@users.noreply.github.com>
2024-05-09 11:23:43 +0200

    test: delete Digital book Test

--- src/test/java/com/iesam/digitallibrary/features/digitalResource/digitalBook/domain/DeleteDigitalBookUseCaseTest.java
+++ src/test/java/com/iesam/digitallibrary/features/digitalResource/digitalBook/domain/DeleteDigitalBookUseCaseTest.java
@@ -7,9 +7,6 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.awt.print.Book;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class DeleteDigitalBookUseCaseTest {
         DeleteDigitalBookUseCase deleteDigitalBookUseCase;
